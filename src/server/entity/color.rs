28c16df8a39a8151179c5cb3ede557c2d51c699c
Vishal <vishal@Aizadas-MBP.fios-router.home>
2023-10-29 14:59:28 -0400

    Add impl and test for moves for token. A token goes though Home, Running, FinalWalk and Done status.

--- src/server/entity/color.rs
+++ src/server/entity/color.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+/// A color of token - we have a board and color in clockwise is Yellow, Blue, Red, Green.
+/// For our implementation, we take yello as first quardent.
+///
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type")]
 pub enum Color {
@@ -7,4 +10,30 @@ pub enum Color {
     Blue,
     Red,
     Green,
+}
+
+impl Color {
+    /// As yellow is first quardent its offset is 0.
+    /// All other color gets +13 offset for each clockwise distance from yellow.
+    pub fn pos_offset(&self) -> u32 {
+        match &self {
+            Color::Yellow => 0,
+            Color::Blue => 13,
+            Color::Red => 26,
+            Color::Green => 39,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_pos_offset() {
+        assert_eq!(0, Color::Yellow.pos_offset());
+        assert_eq!(13, Color::Blue.pos_offset());
+        assert_eq!(26, Color::Red.pos_offset());
+        assert_eq!(39, Color::Green.pos_offset());
+    }
 }
--- src/server/entity/token.rs
+++ src/server/entity/token.rs
@@ -1,18 +1,132 @@
 use serde::{Deserialize, Serialize};
 
 use super::color::Color;
+use anyhow::{bail, Result};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// There are 4 token of each color. Their ids are b/w 1 - 4.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Token {
     color: Color,
     id: u8,
     status: Status,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+/// Status of token.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type")]
 pub enum Status {
+    /// Token can not be used yet, and they can only be used if dice says 6 and player chooses to bring them out.
     Home,
-    Running { x: u8, y: u8 },
+    /// Home token transition to Running after getting 6 points. They get 50 position to move.
+    Running {
+        pos: u8,
+    },
+    /// After 50 moves, they have 6 moves left. But now they are out of any danger.
+    FinalWalk {
+        pos: u8,
+    },
     Done,
+}
+
+impl Token {
+    pub fn new(color: Color, id: u8) -> Self {
+        assert!(id >= 1 && id <= 4);
+        Token {
+            color,
+            id,
+            status: Status::Home,
+        }
+    }
+
+    fn with_staus(self, status: Status) -> Self {
+        Token {
+            color: self.color,
+            id: self.id,
+            status,
+        }
+    }
+
+    // fn with_pos(self, pos: u8) -> Self {
+    //     let new_staus = match self.status {
+    //         Status::Home => panic!("Home can not have pos"),
+    //         Status::Running { .. } => Status::Running { pos },
+    //         Status::FinalWalk { .. } => Status::FinalWalk { pos },
+    //         Status::Done => panic!("Done can not have pos"),
+    //     };
+    //     Token {
+    //         color: self.color,
+    //         id: self.id,
+    //         status: new_staus,
+    //     }
+    // }
+
+    pub fn is_valid_move(&self, count: u8) -> bool {
+        match self.status {
+            Status::Home => count == 6,
+            Status::Running { pos } => pos + count <= 57,
+            Status::FinalWalk { pos } => pos + count <= 6,
+            Status::Done => false,
+        }
+    }
+
+    pub fn move_token(&mut self, count: u8) -> Result<()> {
+        if !self.is_valid_move(count) {
+            bail!("Not valid move for token {:#?} and move {:?}", self, count);
+        }
+        match self.status {
+            Status::Home => self.status = Status::Running { pos: 1 },
+            Status::Running { pos } => {
+                let total = pos + count;
+                if total <= 51 {
+                    self.status = Status::Running { pos: total };
+                } else {
+                    // Transition to final walk
+                    self.status = Status::FinalWalk { pos: 1 };
+                    // and then take left steps.
+                    self.move_token(total - 52).expect(&format!(
+                        "This should not happen with {:?} and move {:?}",
+                        self,
+                        total - 52
+                    ));
+                }
+            }
+            Status::FinalWalk { pos } => {
+                let total = pos + count;
+                if total == 6 {
+                    self.status = Status::Done;
+                } else {
+                    self.status = Status::FinalWalk { pos: total }
+                }
+            }
+            Status::Done => bail!("Done status does not have any transition"),
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[should_panic]
+    fn panic_for_invalid_token_id(#[values(0, 5)] value: u8) {
+        Token::new(Color::Yellow, value);
+    }
+
+    #[rstest]
+    #[case((Token::new(Color::Red, 1), 6), Token::new(Color::Red, 1).with_staus(Status::Running { pos: 1 }))]
+    #[case((Token::new(Color::Red, 1).with_staus(Status::Running { pos: 10 }), 6), Token::new(Color::Red, 1).with_staus(Status::Running { pos: 16 }))]
+    #[case((Token::new(Color::Red, 1).with_staus(Status::Running { pos: 49 }), 3), Token::new(Color::Red, 1).with_staus(Status::FinalWalk { pos: 1 }))]
+    #[case((Token::new(Color::Red, 1).with_staus(Status::Running { pos: 51 }), 6), Token::new(Color::Red, 1).with_staus(Status::Done))]
+    #[case((Token::new(Color::Red, 1).with_staus(Status::FinalWalk { pos: 2 }), 4), Token::new(Color::Red, 1).with_staus(Status::Done))]
+    #[case((Token::new(Color::Red, 1).with_staus(Status::FinalWalk { pos: 2 }), 3), Token::new(Color::Red, 1).with_staus(Status::FinalWalk { pos: 5 }))]
+
+    fn test_valid_moves(#[case] input: (Token, u8), #[case] expected: Token) {
+        let (mut current, count) = input;
+        current.move_token(count).unwrap();
+        assert_eq!(current, expected);
+    }
 }
